5d8d3d91b4c200800238b3f4b3d8b08de39fadfa
nialdaly <nial@cofide.io>
2025-05-30 19:43:09 +0100

    Bump the cofide-trust-zone-server image tag

--- federated-cofidectl-tf-eks.sh
+++ federated-cofidectl-tf-eks.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-# This script creates a pair of kind clusters and defines trust zones,
+# This script uses two existing EKS clusters and defines trust zones,
 # clusters, an attestation policy, bindings and federations in the staging
 # Connect using cofidectl and terraform-provider-cofide.
-# It then runs a ping-pong test between the trust zones, each using the Cofide trust zone server.
+# It then runs a ping-pong test between the trust zones.
 
 # Prerequisites: ./prerequisites.sh
 
@@ -16,8 +16,8 @@ source config.env
 export REGISTRY=010438484483.dkr.ecr.eu-west-1.amazonaws.com
 export REPOSITORY=cofide/trust-zone-server
 export TAG=v1.10.11
-export CONNECT_URL
-export CONNECT_TRUST_DOMAIN
+export CONNECT_URL=$CONNECT_URL
+export CONNECT_TRUST_DOMAIN=$CONNECT_TRUST_DOMAIN
 
 BUNDLE_ID=$(echo $CONNECT_TRUST_DOMAIN | cut -d '.' -f 1)
 export CONNECT_BUNDLE_ENDPOINT_URL="https://$CONNECT_BUNDLE_HOST/$BUNDLE_ID/bundle"
@@ -26,7 +26,6 @@ export CONNECT_BUNDLE_ENDPOINT_URL="https://$CONNECT_BUNDLE_HOST/$BUNDLE_ID/bund
 UNIQUE_ID=$(uuidgen | head -c 8 | tr A-Z a-z)
 
 WORKLOAD_K8S_CLUSTER_NAME_1="workload-${UNIQUE_ID}-1"
-WORKLOAD_K8S_CLUSTER_CONTEXT_1="kind-workload-${UNIQUE_ID}-1"
 # Trust zones must be unique within a single Cofide Connect service.
 WORKLOAD_TRUST_ZONE_1="${UNIQUE_ID}-1"
 # Trust domains must currently be globally unique due to a shared S3 bucket for hosting bundles.
@@ -38,7 +37,6 @@ export TRUST_DOMAIN=$WORKLOAD_TRUST_DOMAIN_1
 envsubst < templates/trust-zone-server-values.yaml > generated/trust-zone-server-values-${WORKLOAD_TRUST_ZONE_1}.yaml
 
 WORKLOAD_K8S_CLUSTER_NAME_2="workload-${UNIQUE_ID}-2"
-WORKLOAD_K8S_CLUSTER_CONTEXT_2="kind-workload-${UNIQUE_ID}-2"
 # Trust zones must be unique within a single Cofide Connect service.
 WORKLOAD_TRUST_ZONE_2="${UNIQUE_ID}-2"
 # Trust domains must currently be globally unique due to a shared S3 bucket for hosting bundles.
@@ -49,19 +47,13 @@ export TRUST_DOMAIN=$WORKLOAD_TRUST_DOMAIN_2
 
 envsubst < templates/trust-zone-server-values.yaml > generated/trust-zone-server-values-${WORKLOAD_TRUST_ZONE_2}.yaml
 
-kind delete cluster --name $WORKLOAD_K8S_CLUSTER_NAME_1
-kind delete cluster --name $WORKLOAD_K8S_CLUSTER_NAME_2
+AWS_REGION=eu-west-1 envsubst <templates/ebs-storageclass-template.yaml >generated/ebs-storageclass.yaml
 
-# Patch in host Docker config in order to enable pulling images
-# to the Kind cluster. This envsubst approach is required as Kind does
-# not support ~ or $HOME directly in the extraMounts attribute of the config
-# https://github.com/kubernetes-sigs/kind/issues/3642
-export PATH_TO_HOST_DOCKER_CREDENTIALS=$HOME/.docker/config.json
-envsubst < templates/kind_workload_config_template.yaml > generated/kind_workload_config.yaml
-kind create cluster --name $WORKLOAD_K8S_CLUSTER_NAME_1 --config generated/kind_workload_config.yaml
-kind create cluster --name $WORKLOAD_K8S_CLUSTER_NAME_2 --config generated/kind_workload_config.yaml
+# Create an EBS storageclass in each EKS cluster for the associated trust zone server.
+kubectl apply -f generated/ebs-storageclass.yaml --context $WORKLOAD_K8S_CLUSTER_CONTEXT_1
+kubectl apply -f generated/ebs-storageclass.yaml --context $WORKLOAD_K8S_CLUSTER_CONTEXT_2
 
-# Applies the RBAC needed by the trust zone server in each Kind cluster.
+# Applies the RBAC needed by the trust zone server in each EKS cluster.
 kubectl apply -f templates/trust-zone-server-rbac.yaml --context $WORKLOAD_K8S_CLUSTER_CONTEXT_1
 kubectl apply -f templates/trust-zone-server-rbac.yaml --context $WORKLOAD_K8S_CLUSTER_CONTEXT_2
 
@@ -95,15 +87,16 @@ export TF_VAR_trust_domain_1="${WORKLOAD_TRUST_DOMAIN_1}"
 export TF_VAR_cluster_1_name="${WORKLOAD_K8S_CLUSTER_NAME_1}"
 export TF_VAR_cluster_1_kubernetes_context="${WORKLOAD_K8S_CLUSTER_CONTEXT_1}"
 export TF_VAR_cluster_1_extra_helm_values="$(realpath generated/trust-zone-server-values-${WORKLOAD_TRUST_ZONE_1}.yaml)"
-
-export TF_VAR_attestation_policy_name="${NAMESPACE}-ns-${UNIQUE_ID}"
-export TF_VAR_attestation_policy_namespace="${NAMESPACE}"
+export TF_VAR_attestation_policy_1_name="${NAMESPACE-ns-$WORKLOAD_TRUST_ZONE_1}"
+export TF_VAR_attestation_policy_1_namespace="${NAMESPACE}"
 
 export TF_VAR_trust_zone_2_name="${WORKLOAD_TRUST_ZONE_2}"
 export TF_VAR_trust_domain_2="${WORKLOAD_TRUST_DOMAIN_2}"
 export TF_VAR_cluster_2_name="${WORKLOAD_K8S_CLUSTER_NAME_2}"
 export TF_VAR_cluster_2_extra_helm_values="$(realpath generated/trust-zone-server-values-${WORKLOAD_TRUST_ZONE_2}.yaml)"
 export TF_VAR_cluster_2_kubernetes_context="${WORKLOAD_K8S_CLUSTER_CONTEXT_2}"
+export TF_VAR_attestation_policy_name="${NAMESPACE-ns-$UNIQUE_ID}"
+export TF_VAR_attestation_policy_namespace="${NAMESPACE}"
 
 terraform -chdir=./terraform/federated init -input=false -backend=false
 
@@ -133,8 +126,8 @@ fi
 echo "Server deployment complete"
 
 echo "Discovering server IP..."
-kubectl --context "$SERVER_CTX" wait --for=jsonpath="{.status.loadBalancer.ingress[0].ip}" service/ping-pong-server -n $NAMESPACE --timeout=60s
-export PING_PONG_SERVER_SERVICE_HOST=$(kubectl --context "$SERVER_CTX" get service ping-pong-server -n $NAMESPACE -o "jsonpath={.status.loadBalancer.ingress[0].ip}")
+kubectl --context "$SERVER_CTX" wait --for=jsonpath="{.status.loadBalancer.ingress[0].hostname}" service/ping-pong-server -n $NAMESPACE --timeout=60s
+export PING_PONG_SERVER_SERVICE_HOST=$(kubectl --context "$SERVER_CTX" get service ping-pong-server -n $NAMESPACE -o "jsonpath={.status.loadBalancer.ingress[0].hostname}")
 echo "Server is $PING_PONG_SERVER_SERVICE_HOST"
 
 CLIENT_MANIFEST="$COFIDE_DEMOS_BRANCH/workloads/ping-pong/ping-pong-client/deploy.yaml"
