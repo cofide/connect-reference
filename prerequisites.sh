7f6a27bea778d14f0b1d254805fe3953d010a579
Alec Wilson <alec@cofide.io>
2025-10-28 15:58:42 +0000

    fix: Get multi-mesh example working with Cofide spire and sidecar istio containers

--- prerequisites.sh
+++ prerequisites.sh
@@ -11,4 +11,13 @@ for cmd in aws cofidectl curl docker helm kubectl uuidgen; do
   fi
 done
 
+# Minimum kind version so default Kubernetes cluster version includes sidecar containers
+# https://kubernetes.io/blog/2025/04/23/kubernetes-v1-33-release/#stable-sidecar-containers
+min_version="0.30.0"
+current_version=$(kind version | awk '{print $2}' | sed 's/^v//')
+if ! printf '%s\n%s\n' "$min_version" "$current_version" | sort -V -C &>/dev/null; then
+  echo "Kind version v${min_version} or higher is required, but found v${current_version}."
+  exit 1
+fi
+
 ./login.sh
